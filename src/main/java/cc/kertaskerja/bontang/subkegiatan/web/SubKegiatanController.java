f0198319f2eb2177f131c80165328fd61c720c1b
Affandi Pratama <affandipratama45@gmail.com>
2025-11-25 07:26:52 +0700

    Fix import controller

--- src/main/java/cc/kertaskerja/bontang/subkegiatan/web/SubKegiatanController.java
+++ src/main/java/cc/kertaskerja/bontang/subkegiatan/web/SubKegiatanController.java
@@ -6,13 +6,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
-@Controller
+@RestController
 @Tag(name = "subkegiatan")
 @RequestMapping("subkegiatan")
 public class SubKegiatanController {
