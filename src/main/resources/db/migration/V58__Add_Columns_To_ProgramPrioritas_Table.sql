7f2618421829b9515afba4ec3c827da0d650e910
Affandi Pratama <affandipratama45@gmail.com>
2026-01-14 12:20:52 +0700

    Fix migrasi error program prioritas

--- src/main/resources/db/migration/V58__Add_Columns_To_ProgramPrioritas_Table.sql
+++ src/main/resources/db/migration/V58__Add_Columns_To_ProgramPrioritas_Table.sql
@@ -1,12 +1,15 @@
 ALTER TABLE program_prioritas
     ADD COLUMN id_sub_kegiatan_opd BIGINT,
-    ADD COLUMN tahun INTEGER NOT NULL,
+    ADD COLUMN tahun INTEGER NOT NULL DEFAULT 1,
     ADD COLUMN keterangan VARCHAR(255),
     ADD COLUMN periode_tahun_awal INTEGER,
     ADD COLUMN periode_tahun_akhir INTEGER,
     ADD COLUMN status VARCHAR(50),
     ADD COLUMN kode_opd VARCHAR(50),
     ADD COLUMN kode_sub_kegiatan_opd VARCHAR(50);
+
+-- Drop default value from tahun column after it's created
+ALTER TABLE program_prioritas ALTER COLUMN tahun DROP DEFAULT;
 
 ALTER TABLE program_prioritas
     ADD CONSTRAINT fk_program_prioritas_subkegiatan_opd
