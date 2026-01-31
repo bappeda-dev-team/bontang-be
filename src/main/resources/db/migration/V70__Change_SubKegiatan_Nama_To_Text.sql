1c47d87c386c19f0c04518ce203eccbcd86fa9ba
Affandi Pratama <affandipratama45@gmail.com>
2026-01-31 18:18:06 +0700

    Ubah tipe data nama sub kegiatan menjadi text

---
+++ src/main/resources/db/migration/V70__Change_SubKegiatan_Nama_To_Text.sql
@@ -0,0 +1,8 @@
+ALTER TABLE subkegiatan
+    ALTER COLUMN nama_subkegiatan TYPE TEXT;
+
+ALTER TABLE subkegiatan_opd
+    ALTER COLUMN nama_subkegiatan TYPE TEXT;
+
+ALTER TABLE subkegiatan_rencana_kinerja
+    ALTER COLUMN nama_subkegiatan TYPE TEXT;
